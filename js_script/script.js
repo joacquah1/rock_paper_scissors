238c57a7894e5177756f4e5322cd40d213b4343a
Jephtah Ogyefo Acquah <ogyefo.acquah@gmail.com>
2023-09-27 09:32:16 +0000

    Modified the playRound() functions making it short by reducing the number of the conditional statements use  and while making it readable with the aid of the logical operator, And => ||

--- js_script/script.js
+++ js_script/script.js
@@ -1,51 +1,35 @@
 
-
+// getComputerChoice() function allows computer to make a random choice between rock paper and scissors.
 function getComputerChoice(){
     gameOptions=["Rock","Paper","Scissors"]
     let computerShoot = gameOptions[Math.floor((Math.random())*3)];
     return computerShoot;
 }
-//consolgetComputerChoice());
-/*loses scissors against rock,rock against paper,paper against scissors */
+//playRound() function takes the user's choices or shoot, compares with the computer's choice, and determine
+//who won each of the five rounds and calculate the overall score for both players and finally determine 
+//who wins the overal game scissors loses against rock,rock loses against paper,paper loses against scissors.
 function playRound(playerSelection , computerSelection){
-    
     let userScore=0;
     let computerScore=0;
     for(i=1;(i<=5);i++){ 
         playerSelection= prompt("Which one will you shoot? Rock, Paper or Scissors!","paper")
         computerSelection= getComputerChoice();
-        if (playerSelection.toLowerCase()==="rock" && computerSelection.toLowerCase() ==="paper"){
-            computerScore+=1;
-            console.log("You Lose! Paper beats Rock");
-            
-        }
-        else if ((playerSelection.toLowerCase()==="scissors") && computerSelection.toLowerCase() ==="rock"){
-            computerScore+=1;
 
-            console.log("You Lose! Rock beats Scissors ");
-            
-        }
-        else if (playerSelection.toLowerCase()==="paper" && computerSelection.toLowerCase() ==="scissors"){
+        if (
+           (playerSelection.toLowerCase()=== "rock" && computerSelection.toLowerCase() ===     "paper") 
+        || (playerSelection.toLowerCase()=== "scissors" && computerSelection.toLowerCase() === "rock")
+        || (playerSelection.toLowerCase()=== "paper" && computerSelection.toLowerCase() ===    "scissors")){
             computerScore+=1;
-            console.log("You Lose! Scissors beats Paper ");
+            console.log(`You Lose! ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}`);
             
-        }
-        else if (playerSelection.toLowerCase()===("paper") && computerSelection.toLowerCase() ==="rock"){
+        }else if (
+            (playerSelection.toLowerCase()=== "paper" && computerSelection.toLowerCase() ===   "rock") 
+        ||  (playerSelection.toLowerCase()=== "rock" && computerSelection.toLowerCase() ===    "scissors") 
+        ||  (playerSelection.toLowerCase()=== "scissors" && computerSelection.toLowerCase()=== "paper")){
             userScore+=1;
-            console.log("You Win! Paper beats Rock ");
+            console.log(` You Win! ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()} `);
             
-        }
-        else if (playerSelection.toLowerCase()===("rock") && computerSelection.toLowerCase() ==="scissors"){
-            userScore+=1;
-            console.log("You Win! Rock beats Scissors ");
-            
-        }
-        else if (playerSelection.toLowerCase()==="scissors" && computerSelection.toLowerCase()==="paper"){
-            userScore+=1;
-            console.log("You Win! Scissors beats Paper");
-            
-        }
-        else {
+        }else {
             console.log( `Tie! Your shot of ${playerSelection} is same as ${computerSelection.toLowerCase()} that computer shot `);
         }
     }
@@ -54,39 +38,8 @@ function playRound(playerSelection , computerSelection){
     return (userScore>computerScore)? "You Win":
             (userScore<computerScore)?"You Lost to Computer":"Phew, Tie match";
 }
-
-
-
+//game() function is used to call and run the playRound() function
 function game(){
-    
-    
     return playRound();
-    
-    
-
 }
 console.log(game())
-/*
-let x=0;
-let y=0;
-let z=0;
-for(i=1;(i<=5);i++){
-    
-    a=Math.floor(Math.random()*100);
-    b=Math.floor(Math.random()*100);
-    c=Math.floor(Math.random()*100);
-    if (a>b&&a>c){
-        x+=1;
-        console.log(`${i} is for a`)
-    }
-    else if(b>a&&b>c){
-        y+=1;
-        console.log(`${i} is for b`)
-    }
-    else if (c>a&&c>b){
-        z+=1
-        console.log(`${i} is for c`)
-    }
-    
-}
-console.log(`result is a had ${x} , b had ${y} ,c had ${z}` );*/
