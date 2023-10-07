0131a35fd864a7ee160dc684536c3616762902ee
Jephtah Ogyefo Acquah <ogyefo.acquah@gmail.com>
2023-10-07 09:35:01 +0000

    Add interactive UI which recieves users clicks from buttons,calls the playRound function add displays the running score with the winner of the game once one player reaches 5 points

--- js_script/script.js
+++ js_script/script.js
@@ -1,3 +1,17 @@
+const rock= document.querySelector('.rock');
+const paper =document.querySelector('.paper');
+const scissors= document.querySelector('.scissors');
+const gameResult = document.querySelector(".gameResult");
+const computerScoreDisplay = document.querySelector('.computerScore');
+const userScoreDisplay = document.querySelector('.userScore');
+
+rock.addEventListener("click",()=>{playRound(rock.textContent,getComputerChoice())});
+paper.addEventListener('click',()=>playRound(paper.textContent,getComputerChoice()));
+scissors.addEventListener('click', ()=>playRound(scissors.textContent,getComputerChoice()));
+let userScore=0;
+let computerScore=0;
+computerScoreDisplay.textContent =  `Computer : ${computerScore}`;
+userScoreDisplay.textContent =`Yours : ${userScore}`;
 
 // getComputerChoice() function allows computer to make a random choice between rock paper and scissors.
 function getComputerChoice(){
@@ -7,39 +21,51 @@ function getComputerChoice(){
 }
 //playRound() function takes the user's choices or shoot, compares with the computer's choice, and determine
 //who won each of the five rounds and calculate the overall score for both players and finally determine 
-//who wins the overal game scissors loses against rock,rock loses against paper,paper loses against scissors.
+//who wins the overall game scissors loses against rock,rock loses against paper,paper loses against scissors.
 function playRound(playerSelection , computerSelection){
-    let userScore=0;
-    let computerScore=0;
-    for(i=1;(i<=5);i++){ 
-        playerSelection= prompt("Which one will you shoot? Rock, Paper or Scissors!","paper")
         computerSelection= getComputerChoice();
-
-        if (
+        if(userScore===5 || computerScore===5){
+            let message = (userScore>computerScore)? "You Win Congratulations":
+            (userScore < computerScore)?"You Lost to Computer":"Phew, Tie match";
+            gameResult.textContent = message;
+        }else if(
            (playerSelection.toLowerCase()=== "rock" && computerSelection.toLowerCase() ===     "paper") 
         || (playerSelection.toLowerCase()=== "scissors" && computerSelection.toLowerCase() === "rock")
         || (playerSelection.toLowerCase()=== "paper" && computerSelection.toLowerCase() ===    "scissors")){
-            computerScore+=1;
-            console.log(`You Lose! ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}`);
+            if (computerScore < 4){
+                computerScoreDisplay.textContent=`Computer : ${++computerScore}`;
+                gameResult.textContent = `You Lose! ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}`;
+        }   else{
+                computerScoreDisplay.textContent=`Computer : ${++computerScore}`;
+                gameResult.textContent = `You Lost to Computer`;
+            }
+               
+            
             
         }else if (
             (playerSelection.toLowerCase()=== "paper" && computerSelection.toLowerCase() ===   "rock") 
         ||  (playerSelection.toLowerCase()=== "rock" && computerSelection.toLowerCase() ===    "scissors") 
         ||  (playerSelection.toLowerCase()=== "scissors" && computerSelection.toLowerCase()=== "paper")){
-            userScore+=1;
-            console.log(` You Win! ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()} `);
-            
+            if(userScore<4){
+                userScoreDisplay.textContent =`Yours : ${++userScore}`;
+                gameResult.textContent = ` You Win! ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()} `;}
+            else{
+                userScoreDisplay.textContent=`Yours : ${++userScore}`;
+                gameResult.textContent = "You Win Congratulations";
+            }
         }else {
-            console.log( `Tie! Your shot of ${playerSelection} is same as ${computerSelection.toLowerCase()} that computer shot `);
+            gameResult.textContent = `Tie! Your shot of ${playerSelection} is same as ${computerSelection.toLowerCase()} that computer shot `;
         }
+        
     }
     
-    console.log (`You got ${userScore} and Computer scored ${computerScore}`);
-    return (userScore>computerScore)? "You Win":
-            (userScore<computerScore)?"You Lost to Computer":"Phew, Tie match";
-}
+    //console.log (`You got ${userScore} and Computer scored ${computerScore}`);
+   // return (userScore>computerScore)? "You Win":
+            //(userScore<computerScore)?"You Lost to Computer":"Phew, Tie match";
+
 //game() function is used to call and run the playRound() function
-function game(){
-    return playRound();
-}
-console.log(game())
+
+   
+
+
+
